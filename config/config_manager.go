e4ab8221e4f0198e86fad4d8d929ef63f5032c19
lrsec <lrsecly@gmail.com>
2018-01-05 19:24:27 +0800

    update

--- config/config_manager.go
+++ config/config_manager.go
@@ -4,22 +4,56 @@
 package config
 
 import (
-	"encoding/json"
-	errors "github.com/lrsec/errors/wrapper"
 	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+	log "github.com/sirupsen/logrus"
 )
 
-func LoadConfig(fileName string, config interface{}) error {
-	file, err := os.Open(fileName)
-	if err != nil {
-		return errors.New(err)
+const (
+	BILL_RUN_MODE_ENV = "BILL_RUN_MODE"
+)
+
+var (
+	configPath string
+	runmode    string
+)
+
+func init() {
+	var err error
+
+	hasRunmode := false
+	if runmode, hasRunmode = os.LookupEnv(BILL_RUN_MODE_ENV); !hasRunmode {
+		panic("Can not find environment BILL_RUN_MODE")
 	}
 
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(config)
+	workPath, err := os.Getwd()
 	if err != nil {
-		return errors.New(err)
+		log.Error("Can not find workpath")
+		panic(err)
 	}
 
-	return nil
+	configPath = filepath.Join(workPath, "conf", runmode)
+	log.Info("Load config files under " + configPath)
+}
+
+func getRelativePath(fileName string) string {
+	return filepath.Join(configPath, fileName)
+}
+
+// decode config from file. config must be a pointer
+func Load(config interface{}, confFile string) error {
+	relativePath := getRelativePath(confFile)
+
+	var err error
+
+	_, err = toml.DecodeFile(relativePath, config)
+	if err != nil {
+		log.Infof("decode config file in relative path: %s error. try absolute path. error: %v", err)
+
+		_, err = toml.DecodeFile(confFile, config)
+	}
+
+	return err
 }
